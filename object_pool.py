eced27e01f25aa2656039656e7b4ec200c0bdc45
Michael Gurvich <misha.gurvich@gmail.com>
2017-07-16 15:02:58 -0700

    object pool

--- object_pool.py
+++ object_pool.py
@@ -1,15 +1,22 @@
-import threading
 from threading import Lock, Condition
 from collections import deque
 
-class ObjectPoolGuard(object):
+
+class ObjectPoolContextManager(object):
     def __init__(self, object_pool):
         self._op = object_pool
+        self._obj = None
 
-    
+    def __enter__(self):
+        self._obj = self._op.get()
+        return self._obj
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._op.put(self._obj)
+
 
 class ObjectPool(object):
-    def __init__(self, construct, reset, limit=4):
+    def __init__(self, construct, reset=None, limit=4):
         self._construct = construct
         self._reset = reset
 
@@ -19,6 +26,9 @@ class ObjectPool(object):
         self._max_num_objects = limit
         self._num_objects = 0
 
+    def item(self):
+        return ObjectPoolContextManager(self)
+
     def get(self):
         with self._lock:
             if len(self._pool) != 0:
@@ -27,7 +37,7 @@ class ObjectPool(object):
             # no objects in the pool
 
             if self._num_objects < self._max_num_objects:
-                obj = construct()
+                obj = self._construct()
                 self._num_objects += 1
                 return obj
 
@@ -40,7 +50,9 @@ class ObjectPool(object):
             return self._pool.pop()
 
     def put(self, obj):
-        self._reset(obj)
+        if self._reset is not None:
+            self._reset(obj)
+
         with self._lock:
             self._pool.append(obj)
             self._cond_have_objects.notify()
@@ -54,6 +66,7 @@ class ObjectPool(object):
     def num_objects(self):
         with self._lock:
             return self._num_objects
+
     @property
     def num_available_objects(self):
         with self._lock:
