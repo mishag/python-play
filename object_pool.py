7587830efe4ec67222115988f93d5408b415c54d
Michael Gurvich <misha.gurvich@gmail.com>
2017-07-16 11:02:42 -0700

    Add code

--- object_pool.py
+++ object_pool.py
@@ -10,29 +10,39 @@ class ObjectPool(object):
         self._lock = Lock()
         self._cond_have_objects = Condition(self._lock)
         self._pool = deque()
+        self._max_num_objects = limit
         self._num_objects = 0
-        self._num_available_objects = 0
 
     def get(self):
-        pass
+        with self._lock:
+            if len(self._pool) != 0:
+                return self._pool.pop()
 
-    def put(self):
-        pass
+            # no objects in the pool
+
+            if self._num_objects < self._max_num_objects:
+                obj = construct()
+                self._num_objects += 1
+                return obj
+
+            # no objects left, but reached maximum
+            while len(self._pool) == 0:
+                self._cond_have_objects.wait()
+
+            assert len(self._pool) > 0
+
+            return self._pool.pop()
+
+    def put(self, obj):
+        self._reset(obj)
+        with self._lock:
+            self._pool.append(obj)
+            self._cond_have_objects.notify()
 
     def _inc_num_objects(self):
         with self._lock:
             self._num_objects += 1
             return self._num_objects
-
-    def _inc_num_available_objects(self):
-        with self._lock:
-            self._num_available_objects += 1
-            return self._num_available_objects
-
-    def _dec_num_available_objects(self):
-        with self._lock:
-            self._num_available_objects -= 1
-            return self._num_available_objects
 
     @property
     def num_objects(self):
@@ -41,4 +51,4 @@ class ObjectPool(object):
     @property
     def num_available_objects(self):
         with self._lock:
-            return self._num_available_objects
+            return len(self._pool)
